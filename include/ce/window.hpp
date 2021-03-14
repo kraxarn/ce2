6b425688900ac4d8cc2f673c6aa9a2f6ef2bbdd9
kraxarn <me@kraxarn.com>
2021-03-14 11:55:17 +0100

    window class moved to engine

--- include/ce/window.hpp
+++ include/ce/window.hpp
@@ -1,1 +1,74 @@
 #pragma once
+
+#include <iostream>
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+
+namespace ce
+{
+	/**
+	 * Application window
+	 */
+	class window
+	{
+	public:
+		/**
+		 * Create and show a new window
+		 * @param title Window title
+		 * @param x X position
+		 * @param y Y position
+		 * @param w Width
+		 * @param h Height
+		 */
+		window(const std::string &title, int x, int y, int w, int h);
+
+		/**
+		 * Destroy window and free resources
+		 */
+		~window();
+
+		/**
+		 * Get SDL Renderer
+		 * @note Shouldn't be needed
+		 */
+		SDL_Renderer *get_renderer();
+
+		/**
+		 * Clear window
+		 * @param r Red
+		 * @param g Green
+		 * @param b Blue
+		 * @return True if successful
+		 */
+		bool clear(uint8_t r = 0x00, uint8_t g = 0x00, uint8_t b = 0x00);
+
+		/**
+		 * Render to screen
+		 */
+		void present();
+
+		/**
+		 * Render texture to window
+		 * @param texture Texture to render
+		 * @param x X position
+		 * @param y Y position
+		 */
+		void render(SDL_Texture *texture, int x, int y);
+
+		/**
+		 * Width
+		 */
+		int w();
+
+		/**
+		 * Height
+		 */
+		int h();
+
+	private:
+		SDL_Window *sdl_window = nullptr;
+		SDL_Renderer *sdl_renderer = nullptr;
+	};
+}
---
+++ src/window.cpp
@@ -0,0 +1,66 @@
+#include "ce/window.hpp"
+
+#include <SDL.h>
+
+using namespace ce;
+
+window::window(const std::string &title, int x, int y, int w, int h)
+{
+	sdl_window = SDL_CreateWindow(title.c_str(), x, y, w, h, SDL_WINDOW_SHOWN);
+	if (sdl_window == nullptr)
+		throw std::runtime_error(SDL_GetError());
+
+	sdl_renderer = SDL_CreateRenderer(sdl_window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (sdl_renderer == nullptr)
+		throw std::runtime_error(SDL_GetError());
+}
+
+window::~window()
+{
+	if (sdl_renderer != nullptr)
+		SDL_DestroyRenderer(sdl_renderer);
+
+	if (sdl_window != nullptr)
+		SDL_DestroyWindow(sdl_window);
+}
+
+SDL_Renderer *window::get_renderer()
+{
+	return sdl_renderer;
+}
+
+bool window::clear(Uint8 r, Uint8 g, Uint8 b)
+{
+	SDL_SetRenderDrawColor(sdl_renderer, r, g, b, 0xff);
+	return SDL_RenderClear(sdl_renderer);
+}
+
+void window::present()
+{
+	SDL_RenderPresent(sdl_renderer);
+}
+
+void window::render(SDL_Texture *texture, int x, int y)
+{
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+
+	SDL_QueryTexture(texture, nullptr, nullptr, &dst.w, &dst.h);
+	SDL_RenderCopy(sdl_renderer, texture, nullptr, &dst);
+}
+
+int window::w()
+{
+	int w;
+	SDL_GetWindowSize(sdl_window, &w, nullptr);
+	return w;
+}
+
+int window::h()
+{
+	int h;
+	SDL_GetWindowSize(sdl_window, nullptr, &h);
+	return h;
+}
