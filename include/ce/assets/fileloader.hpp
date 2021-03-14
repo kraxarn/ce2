d2b124e03bb89ecefb5664b35c1f3a6f58ca8a45
kraxarn <me@kraxarn.com>
2021-03-14 13:54:15 +0100

    Allow custom folder names

--- include/ce/assets/fileloader.hpp
+++ include/ce/assets/fileloader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fstream>
+#include <utility>
 
 #include "ce/assets/assetloader.hpp"
 #include "ce/format.hpp"
@@ -8,11 +9,21 @@
 namespace ce
 {
 	/**
-	 * Loads assets from file in the data/ folder
+	 * Loads assets from file in a folder
 	 */
 	class file_loader: public asset_loader
 	{
+	public:
+		/**
+		 * Construct a new file loader
+		 * @param folder Folder to load files from
+		 */
+		explicit file_loader(std::string folder);
+
 	protected:
 		std::vector<std::uint8_t> load(const std::string &path) override;
+
+	private:
+		std::string folder_name;
 	};
 }
--- src/fileloader.cpp
+++ src/fileloader.cpp
@@ -2,8 +2,13 @@
 
 using namespace ce;
 
+file_loader::file_loader(std::string folder)
+	: folder_name(std::move(folder))
+{
+}
+
 std::vector<std::uint8_t> file_loader::load(const std::string &path)
 {
-	std::ifstream file(fmt::format("data/{}", path), std::ios::binary);
+	std::ifstream file(fmt::format("{}/{}", folder_name, path), std::ios::binary);
 	return std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
 }
