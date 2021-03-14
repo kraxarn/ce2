a1317459507537b2f7dd8da0ac078eb33fd84e4c
kraxarn <me@kraxarn.com>
2021-03-14 14:14:09 +0100

    Throw exception early if file couldn't be found

--- src/fileloader.cpp
+++ src/fileloader.cpp
@@ -9,6 +9,10 @@ file_loader::file_loader(std::string folder)
 
 std::vector<std::uint8_t> file_loader::load(const std::string &path)
 {
-	std::ifstream file(fmt::format("{}/{}", folder_name, path), std::ios::binary);
+	auto file_path = fmt::format("{}/{}", folder_name, path);
+	std::ifstream file(file_path, std::ios::binary);
+	if (!file.is_open())
+		throw std::runtime_error(fmt::format("failed to load file: \"{}\"", file_path));
+
 	return std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
 }
