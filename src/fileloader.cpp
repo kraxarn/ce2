a962e21e2bda2768b6b4ff93b18039fdbae56b98
kraxarn <me@kraxarn.com>
2021-03-14 21:33:17 +0100

    Make sure file is good too

--- src/fileloader.cpp
+++ src/fileloader.cpp
@@ -11,7 +11,7 @@ std::vector<std::uint8_t> file_loader::load(const std::string &path)
 {
 	auto file_path = fmt::format("{}/{}", folder_name, path);
 	std::ifstream file(file_path, std::ios::binary);
-	if (!file.is_open())
+	if (!file.is_open() || file.bad())
 		throw std::runtime_error(fmt::format("failed to load file: \"{}\"", file_path));
 
 	return std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
